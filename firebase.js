a79190cd6ec9a7bff8a1e8d799c7d8a43eace228
MSTERPEGXSUS <anshulpanda54@gmail.com>
2026-06-07 16:14:41 +0100

    Fix import formatting and ensure consistent export

--- firebase.js
+++ firebase.js
@@ -1,11 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsFW_ItFdK_uXs1UNvOhfbLdWKLccEfpk",
+      apiKey: "AIzaSyCsFW_ItFdK_uXs1UNvOhfbLdWKLccEfpk",
   authDomain: "infinity-cubes-e6bdb.firebaseapp.com",
   projectId: "infinity-cubes-e6bdb",
   storageBucket: "infinity-cubes-e6bdb.firebasestorage.app",
@@ -14,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
 export { db };
